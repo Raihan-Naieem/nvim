5eb6913eb7d6f2bcec54d12745ba90992d08e702
Raihan-Naieem <naieemraiyan6@gmail.com>
2026-07-12 19:32:35 +0600

    added css color viewer

---
+++ lua/plugins/UI/css-colorViewer.lua
@@ -0,0 +1,8 @@
+return {
+  'Godswill-255/colorviewer.nvim',
+  config = function()
+    require('colorviewer').setup {
+      symbol = '■',
+    }
+  end,
+}
--- lua/plugins/UI/init.lua
+++ lua/plugins/UI/init.lua
@@ -2,4 +2,5 @@ return {
   require 'plugins.UI.todo-comments',
   require 'plugins.UI.colorscheme',
   require 'plugins.UI.theme-changer',
+  require 'plugins.UI.css-colorViewer',
 }
